ceb51b8951d659c84b8e4a4562626f941d737d44
sqrt <sqrt@octoblu.com>
2016-06-17 17:11:25 -0700

    v1.0.4 Log base64 image on failure

--- command.js
+++ command.js
@@ -3,7 +3,8 @@
 var system = require('system');
 var casper = require('casper').create({
   onError: (function(error){
-    casper.echo("failure due to error: " + error)
+    console.log("failure due to error: " + error)
+    console.log(this.echo(casper.captureBase64('png')))
     casper.exit(1)
   })
 });
@@ -12,7 +13,10 @@ var casper = require('casper').create({
 var GOOGLE_USERNAME = system.env.GOOGLE_USERNAME;
 var GOOGLE_PASSWORD = system.env.GOOGLE_PASSWORD;
 
-if(!GOOGLE_USERNAME || !GOOGLE_PASSWORD) casper.die('Missing required env: GOOGLE_USERNAME or GOOGLE_PASSWORD')
+if(!GOOGLE_USERNAME || !GOOGLE_PASSWORD) {
+  console.log('Missing required env: GOOGLE_USERNAME or GOOGLE_PASSWORD')
+  this.exit(1)
+}
 
 casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36');
 casper.start('https://app.octoblu.com/');
@@ -56,7 +60,9 @@ casper.waitForText("dashboard", function(){
   this.echo("success");
   this.exit()
 }, function(){
-  this.die("failure")
+  console.log("failure")
+  console.log(this.echo(casper.captureBase64('png')))
+  this.exit(1)
 })
 
 casper.run();
